134bab851eebfb5a8941be8531b7331c930db1c0
Andy Taylor <andytlr@gmail.com>
2015-11-03 21:40:00 -0800

    Stuff in scroll view

--- dropbox-mailbox/ViewController.swift
+++ dropbox-mailbox/ViewController.swift
@@ -9,10 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    @IBOutlet weak var getMeToZero: UIImageView!
+    
+    @IBOutlet weak var search: UIImageView!
+    
+    @IBOutlet weak var message: UIImageView!
+    
+    @IBOutlet weak var otherMessages: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        scrollView.contentSize.width = otherMessages.frame.width
+        scrollView.contentSize.height = otherMessages.frame.height + message.frame.height + search.frame.height + search.frame.height
     }
 
     override func didReceiveMemoryWarning() {
